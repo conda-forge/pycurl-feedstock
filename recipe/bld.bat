cc8c5a6b9234d11761d48ef94762394b3d3d6369
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2024-02-22 13:48:12 +0000

    Use pip to install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,7 @@
-%PYTHON% setup.py install --curl-dir=%LIBRARY_PREFIX% --use-libcurl-dll
+set "PYCURL_CURL_DIR=%LIBRARY_PREFIX%"
+set "PYCURL_USE_LIBCURL_DLL=1"
+
+%PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
 
 rd /s /q %PREFIX%\Doc

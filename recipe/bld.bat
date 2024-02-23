b0e63179c4a53236316a5cead47b5afa871af1a2
Tom Schoonjans <Tom.Schoonjans@genomicsplc.com>
2024-02-23 13:47:12 +0000

    Windows: use correct ssl import library (part 2)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 set "PYCURL_CURL_DIR=%LIBRARY_PREFIX%"
 set "PYCURL_USE_LIBCURL_DLL=1"
 set "PYCURL_SSL_LIBRARY=openssl"
-set "PYCURL_OPENSSL_LIB_NAME=libssl.lib"
+set "PYCURL_OPENSSL_LIB_NAME=libcrypto.lib"
 
 %PYTHON% -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
